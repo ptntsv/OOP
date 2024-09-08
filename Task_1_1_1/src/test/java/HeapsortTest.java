9654755de21c419e21dd7db78c1cbd09f8d93ebc
ptntsv <hitogavax@ya.ru>
2024-09-08 10:05:48 +0700

    Change array brackets

--- Task_1_1_1/src/test/java/HeapsortTest.java
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -16,63 +16,63 @@ public class HeapsortTest {
 
     @Test
     public void testEmpty() {
-        int arr[] = new int[] {};
+        int[] arr = new int[] {};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testSingleton() {
-        int arr[] = new int[] {5};
+        int[] arr = new int[] {5};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testSorted() {
-        int arr[] = new int[] {1, 2, 3, 4, 5};
+        int[] arr = new int[] {1, 2, 3, 4, 5};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testReverseSorted() {
-        int arr[] = new int[] {5, 4, 3, 2, 1};
+        int[] arr = new int[] {5, 4, 3, 2, 1};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testSame() {
-        int arr[] = new int[] {1, 1, 1, 1, 1};
+        int[] arr = new int[] {1, 1, 1, 1, 1};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testAlreadySorted1() {
-        int arr[] = new int[] {2, 3, 4, 5, 6, 1};
+        int[] arr = new int[] {2, 3, 4, 5, 6, 1};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testAlreadySorted2() {
-        int arr[] = new int[] {9, 1, 2, 3, 4, 5};
+        int[] arr = new int[] {9, 1, 2, 3, 4, 5};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testNegatives() {
-        int arr[] = new int[] {-100, -120, -1, -2, -3, -4, -5};
+        int[] arr = new int[] {-100, -120, -1, -2, -3, -4, -5};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
 
     @Test
     public void testNegativesAndPositives() {
-        int arr[] = new int[] {-100, -120, -1, -2, -3, -4, -5};
+        int[] arr = new int[] {-100, -120, -1, -2, -3, -4, -5};
         Main.heapsort(arr);
         Assertions.assertTrue(isSorted(arr));
     }
@@ -80,7 +80,7 @@ public class HeapsortTest {
     @Test
     public void testRandomized() {
         Random rand = new Random();
-        int arr[] = new int[1000];
+        int[] arr = new int[1000];
         for (int i = 0; i < 1000; i++) {
             arr[i] = rand.nextInt();
         }
