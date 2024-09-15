798d71a31c5bea5fd07b0b2aa8448a1ebff87bd3
ptntsv <ptntsv@users.noreply.github.com>
2024-09-15 07:32:52 +0000

    Deploying to gh-pages from @ ptntsv/OOP@abbb6645e6389b4a1c40cb731458fb28b86f02ba 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Card"},{"p":"org.example","l":"CardType"},{"p":"org.example","l":"Dealer"},{"p":"org.example","l":"Deck"},{"p":"org.example","l":"Game"},{"p":"org.example","l":"IO"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Player"},{"p":"org.example","l":"Suit"}]
