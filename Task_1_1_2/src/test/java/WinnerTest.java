8f4fda1bef509b4f9470306ae8c8802c8462640a
ptntsv <hitogavax@ya.ru>
2024-09-17 11:16:30 +0700

    WinnerTest

--- Task_1_1_2/src/test/java/WinnerTest.java
+++ Task_1_1_2/src/test/java/WinnerTest.java
@@ -6,7 +6,10 @@ import org.example.Suit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class BlackJackTest {
+/**
+ * Tests for proper winner choosing.
+ */
+public class WinnerTest {
 
     @Test
     public void testPlayerBlackJack() {
@@ -67,5 +70,26 @@ public class BlackJackTest {
 
         game.checkWinCondition(true);
         Assertions.assertEquals(game.winState, WinState.DRAW);
+    }
+
+    @Test
+    public void testDealer() {
+        Game game = new Game();
+        // Player's init cards
+        Card p1 = new Card(Suit.Spades, CardType.Two);
+        Card p2 = new Card(Suit.Spades, CardType.Four);
+        game.getPlayer().peekCard(p1);
+        game.getPlayer().peekCard(p2);
+        game.getPlayer().peekCard(new Card(Suit.Clubs, CardType.Queen));
+
+        game.getDealer().peekCard(p1);
+        game.getDealer().peekCard(p2);
+        game.getDealer().peekCard(new Card(Suit.Clubs, CardType.Ace));
+
+        game.checkWinCondition(false);
+        Assertions.assertEquals(game.winState, WinState.NEITHER);
+
+        game.checkWinCondition(true);
+        Assertions.assertEquals(game.winState, WinState.DEALER_WON);
     }
 }
