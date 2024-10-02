c4163d7b0c8e086ad754a7536c1e31ae73181ff4
ptntsv <hitogavax@ya.ru>
2024-10-02 20:48:10 +0700

    Binary expression file

--- Task_1_1_3/src/main/java/org/example/Expressions/BinaryExpression.java
+++ Task_1_1_3/src/main/java/org/example/Expressions/BinaryExpression.java
@@ -1,4 +1,26 @@
 package org.example.Expressions;
 
-public class BinaryExpression {
+import java.util.HashMap;
+
+public abstract class BinaryExpression extends Expression {
+
+    public Expression left;
+    public Expression right;
+    public char opSign;
+
+    @Override
+    protected void signify(HashMap<String, Integer> varMap) throws UnsignedVariableException {
+        left.signify(varMap);
+        right.signify(varMap);
+    }
+
+    @Override
+    public String toString() {
+        return '(' + left.toString() + this.opSign + right.toString() + ')';
+    }
+
+    public BinaryExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
 }
