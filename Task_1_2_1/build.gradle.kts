db1b8a6f9870617826d92806fdc16c4be5147bad
ptntsv <hitogavax@ya.ru>
2024-10-15 19:16:16 +0700

    Java version

--- Task_1_2_1/build.gradle.kts
+++ Task_1_2_1/build.gradle.kts
@@ -9,7 +9,11 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
